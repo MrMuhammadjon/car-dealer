9158611172ce69f2367aceffd69a4f8fca4bbb15
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-01-07 12:19:42 +0500

    open side bar

---
+++ script.js
@@ -0,0 +1,17 @@
+let open_Sidebar = document.getElementById('flex-menu');
+let coloe_menu = document.getElementById('close-menu');
+let menu = document.getElementById('open-menu')
+
+open_Sidebar.addEventListener('click', function () {
+    
+    if (menu.classList.contains('open-sidebar-l')) {
+
+        menu.classList.add('open-sidebar-l')
+
+    } else {
+
+        menu.classList.add('open-sidebar-l')
+
+    }
+
+})
